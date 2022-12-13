9440badb637fa4309b862747ebb3dfe5238da81f
IanTaiAhn <iantaiahn@yahoo.com>
2022-12-13 16:39:42 -0700

    Glad I finally figured it out.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,7 @@ import { CryptoService } from './crypto.service';
 export class AppComponent {
   title = 'crypto_api';
   cryptoObj: Array<any> = [];
-  public show0:boolean = false;
   flagArray: Array<any> = [];
-
 
   constructor(private cryptoService: CryptoService) {
     cryptoService.getCryptos().subscribe((cryptos: any) => {
@@ -23,14 +21,7 @@ export class AppComponent {
     })    
   }
 
-  toggle0() {
-    this.show0 = !this.show0;
-    console.log("event fired");
-    // console.log(event.target);
-    // return (event.target as unknown as HTMLInputElement["value"]);
-   }
-
-   iconChange(i: number){
+   expandDetails(i: number){
     this.flagArray[i] = !this.flagArray[i];
     console.log(this.flagArray);
   }
